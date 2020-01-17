c0059d62e624d708373f9919d839e54eaeb011e4
Alec Adub <adub.alec@gmail.com>
2020-01-16 21:19:46 -0500

    Delete tabs, add google-map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleMapComponent } from './components/google-map/google-map.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, GoogleMapComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
