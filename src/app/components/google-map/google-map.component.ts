e6f3cc99f6325492808bbf2da1ffad4d177117d6
Claudia <claudia@bliinx.ca>
2020-01-28 18:19:20 -0500

    can see map with ionic google library on browser

--- src/app/components/google-map/google-map.component.ts
+++ src/app/components/google-map/google-map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
+import { GoogleMaps, GoogleMap } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-google-map',
@@ -7,13 +8,34 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['./google-map.component.scss']
 })
 export class GoogleMapComponent implements OnInit {
-  latitude: number = 45.4946; // to be change with geolocalisation
-  longitude: number = -73.5774;
-  height: number;
+  private latitude: number = 45.4946; // to be change with geolocalisation
+  private longitude: number = -73.5774;
+  private height: number;
+  private map: GoogleMap;
+  private loading: any;
 
-  constructor(platform: Platform) {
+  constructor(
+    private platform: Platform,
+
+  ) {
     this.height = platform.height();
   }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    await this.platform.ready();
+    await this.loadMap();
+  }
+
+  loadMap() {
+    this.map = GoogleMaps.create('map-canvas', {
+      camera: {
+        target: {
+          lat: this.latitude,
+          lng: this.longitude
+        },
+        zoom: 16,
+        tilt: 30
+      }
+    });
+  }
 }
