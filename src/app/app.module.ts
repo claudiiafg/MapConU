223a7c637866083941ebaadfb3f956a1475b3458
Angel Chung <achung998@gmail.com>
2020-02-02 16:14:55 -0500

    Campus change top menu added. No current location option (not implemented)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -21,7 +22,8 @@ import { APIKey } from 'src/environments/env';
     AppRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: APIKey
-    })
+    }),
+    FormsModule
   ],
   providers: [
     StatusBar,
--- src/app/components/google-map/google-map.component.ts
+++ src/app/components/google-map/google-map.component.ts
@@ -7,13 +7,25 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['./google-map.component.scss']
 })
 export class GoogleMapComponent implements OnInit {
+  loc: string;
   latitude: number = 45.4946; // to be change with geolocalisation
   longitude: number = -73.5774;
   height: number;
+
+  //To add default locations
+  locations = [
+    { latitude: 45.495729, longitude: -73.578041 },
+    { latitude: 45.458240, longitude: -73.640452 }
+  ];
 
   constructor(platform: Platform) {
     this.height = platform.height();
   }
 
   ngOnInit() {}
+
+  public changeCampus() {
+    this.latitude = this.locations[this.loc].latitude;
+    this.longitude = this.locations[this.loc].longitude;
+  }
 }
