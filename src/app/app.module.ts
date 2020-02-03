5701d489788f6ed3c4f2eca0f22639801f0d6110
GwenLegate <gwendolyne_l@outlook.com>
2020-02-03 02:17:50 -0500

    made buttons that overlay on map and positioned them.  TODO:proper color and positioning, re configure for indoor map display and remove for settings display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoogleMapComponent } from './components/google-map/google-map.component';
+import { OutdoorNavigationSideButtonsComponent} from "./components/outdoor-navigation-side-buttons/outdoor-navigation-side-buttons.component";
 import { AgmCoreModule } from '@agm/core';
 import { APIKey } from 'src/environments/env';
 
 @NgModule({
-  declarations: [AppComponent, GoogleMapComponent],
+  declarations: [AppComponent, GoogleMapComponent, OutdoorNavigationSideButtonsComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
