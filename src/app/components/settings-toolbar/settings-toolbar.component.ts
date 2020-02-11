ed383d19de77ed78a1fb857f9907eec29c039bea
GwenLegate <gwendolyne_l@outlook.com>
2020-02-11 01:54:20 -0500

    created toolbars for indoor nav and settings. Popover position issue in indoor nav toolbar

---
+++ src/app/components/settings-toolbar/settings-toolbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-toolbar',
+  templateUrl: './settings-toolbar.component.html',
+  styleUrls: ['./settings-toolbar.component.scss'],
+})
+export class SettingsToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
