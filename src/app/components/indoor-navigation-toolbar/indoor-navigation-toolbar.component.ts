0f6d65ec5ec34b333b64a2d3cc2f95d755b7707d
GwenLegate <gwendolyne_l@outlook.com>
2020-02-14 09:15:46 -0500

    bug fix: floorUp and floorDown buttons weren't working properly

--- src/app/components/indoor-navigation-toolbar/indoor-navigation-toolbar.component.ts
+++ src/app/components/indoor-navigation-toolbar/indoor-navigation-toolbar.component.ts
@@ -65,7 +65,9 @@ export class IndoorNavigationToolbarComponent implements OnInit {
   }
 
   changeFloor() {
+    console.log(this.currentFloor + " is current floor");
     this.currentFloor = this.updateFloor;
+    console.log(this.updateFloor + " is update floor");
   }
 
   moveUpFloor() {
