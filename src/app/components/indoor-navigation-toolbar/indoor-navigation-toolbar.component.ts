f578a34de949e1df70b02e1d42e2ae6acadb22df
GwenLegate <gwendolyne_l@outlook.com>
2020-02-19 12:41:38 -0500

    added buildings from Loyola to array. Implemented fix to display basement levels for buildings that have them

--- src/app/components/indoor-navigation-toolbar/indoor-navigation-toolbar.component.ts
+++ src/app/components/indoor-navigation-toolbar/indoor-navigation-toolbar.component.ts
@@ -15,28 +15,39 @@ import {AppComponent} from "../../app.component";
 
 export class IndoorNavigationToolbarComponent implements OnInit {
   building: string;
-  maxFloors: number;
-  currentFloor: number;
-  updateFloor: number;
+  maxFloorIndex: number;
+  minFloorIndex: number;
+  currentFloor: string;
+  currentFloorIndex: number;
+  updateFloor: string;
 
   //number of floors in the building
   //TODO: add all buildings and the floors with floor plans
 
   buildingInfo = [
-    {buildingName: "Hall Building", numFloors: 9},
-    {buildingName: "John Molson Building", numFloors: 4}
+    {buildingName: "Hall Building", topFloorIndex: 12, bottomFloorIndex: 2},
+    {buildingName: "John Molson Building", topFloorIndex: 6, bottomFloorIndex: 0},
+    {buildingName: "Faubourg", topFloorIndex: 2, bottomFloorIndex: 0},
+    {buildingName: "Richard Grey Renaud Science Complex", topFloorIndex: 7, bottomFloorIndex: 1},
+    {buildingName: "Central Building", topFloorIndex: 3, bottomFloorIndex: 2},
+    {buildingName: "Higston Hall", topFloorIndex: 2, bottomFloorIndex: 2},
+    {buildingName: "Communication Studies and Journalism Building ", topFloorIndex: 5, bottomFloorIndex: 2},
+    {buildingName: "Vanier Extension", topFloorIndex: 5, bottomFloorIndex: 2}
   ];
 
   floors = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9
+    "s2",
+    "s1",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
   ];
 
   constructor(private data: DataSharingService) {
@@ -51,32 +62,45 @@ export class IndoorNavigationToolbarComponent implements OnInit {
     //placeholder because logic to come from outdoor-nav is not implemented yet can be deleted when it is
     this.building = "Hall Building";
 
-    //sets max number of allowed floors for the selected building
+    //sets max and min number of allowed floors for the selected building
     let i;
     let length = this.buildingInfo.length;
     for (i = 0; i < length; i++) {
       if (this.buildingInfo[i].buildingName === this.building) {
-        this.maxFloors = this.buildingInfo[i].numFloors;
+        this.maxFloorIndex = this.buildingInfo[i].topFloorIndex;
+        this.minFloorIndex = this.buildingInfo[i].bottomFloorIndex;
         break;
       }
     }
-    this.currentFloor = 1;
-
+    this.currentFloor = "1";
+    this.currentFloorIndex = 2;
   }
 
   changeFloor() {
     this.currentFloor = this.updateFloor;
+    let i;
+    length = this.floors.length;
+
+    for (i = 0; i < length; i++) {
+      if (this.floors[i] === this.currentFloor) {
+        this.currentFloorIndex = i;
+        break;
+      }
+    }
+
   }
 
   moveUpFloor() {
-    if (this.currentFloor < this.maxFloors) {
-      this.currentFloor++;
+    if (this.currentFloorIndex < this.maxFloorIndex) {
+      this.currentFloorIndex++;
+      this.currentFloor = this.floors[(this.currentFloorIndex)];
     }
   }
 
   moveDownFloor() {
-    if (this.currentFloor > 1) {
-      this.currentFloor--;
+    if (this.currentFloorIndex > this.minFloorIndex) {
+      this.currentFloorIndex--;
+      this.currentFloor = this.floors[this.currentFloorIndex];
     }
   }
 }
