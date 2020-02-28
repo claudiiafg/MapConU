d94ab13ceec425280d56df886a46b2ca14079bd7
RobDrury <rdrury1513@hotmail.com>
2020-02-28 12:17:16 -0500

    Added test case for toggling between campuses

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-
+import { browser, by, element, $ } from 'protractor';
 describe('new App', () => {
   let page: AppPage;
 
@@ -7,8 +7,34 @@ describe('new App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getPageTitle()).toContain('Tab One');
+  it('Has MapConU as the Title ', () => {
+    browser.get('/');
+    let x = element(by.id('campus-change')).getText();
+    expect(browser.getTitle()).toContain('MapConU');
   });
+  it('Current Campus should toggle when selected at top of the UI', () => {
+    browser.get('/');
+    //set location to sgw
+    element(by.xpath("/html/body/app-root/ion-app/header/app-outdoor-navigation-toolbar/ion-header/ion-toolbar/ion-item/ion-select")).click();
+    browser.driver.sleep(500);
+    element(by.xpath("/html/body/app-root/ion-app/ion-popover/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[1]")).click();
+
+    //check location is set to SGW
+    expect(element(by.id("campus-change")).getText()).toContain("SGW Campus");
+    expect(element(by.css('ng-reflect-longitude="-73.578041"'))).toBeDefined();
+    expect(element(by.css('ng-reflect-latitude="45.495729"'))).toBeDefined();
+
+    //set location to Loyola
+    element(by.xpath("/html/body/app-root/ion-app/header/app-outdoor-navigation-toolbar/ion-header/ion-toolbar/ion-item/ion-select")).click();
+    browser.driver.sleep(500);
+    element(by.xpath("/html/body/app-root/ion-app/ion-popover/div/div[2]/ion-select-popover/ion-list/ion-radio-group/ion-item[2]")).click();
+
+    //check locations is set to loyla
+    expect(element(by.id("campus-change")).getText()).toContain("Loyola Campus");
+    expect(element(by.css('ng-reflect-longitude="-73.640452"'))).toBeDefined();
+    expect(element(by.css('ng-reflect-latitude="45.45824"'))).toBeDefined();
+
+  });
+
 });
+
