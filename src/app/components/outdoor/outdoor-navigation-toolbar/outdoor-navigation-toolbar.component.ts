245227a450d1d75d8f0489b9c84adeecbb6509cc
GwenLegate <gwendolyne_l@outlook.com>
2020-03-15 00:37:07 -0400

    successfully setup translation services and tested it on outdoor toolbar translation

--- src/app/components/outdoor/outdoor-navigation-toolbar/outdoor-navigation-toolbar.component.ts
+++ src/app/components/outdoor/outdoor-navigation-toolbar/outdoor-navigation-toolbar.component.ts
@@ -31,7 +31,6 @@ export class OutdoorNavigationToolbarComponent implements OnInit, AfterViewInit 
   public walkColor: string = 'yellow';
   public campus1: string;
   public campus2: string;
-  public language: string; //current App language
 
   //Array for lat, long of specific locations
   public locations = [
@@ -40,7 +39,7 @@ export class OutdoorNavigationToolbarComponent implements OnInit, AfterViewInit 
   ];
 
   constructor(
-    private dataSharing: DataSharingService,
+    private data: DataSharingService,
     private events: Events,
     public mapsAPILoader: MapsAPILoader,
     public ngZone: NgZone,
@@ -48,15 +47,12 @@ export class OutdoorNavigationToolbarComponent implements OnInit, AfterViewInit 
     private router: Router,
     private translate: TranslationService
   ) {
-    this.dataSharing.currentMessage.subscribe(
+    this.data.currentMessage.subscribe(
       incomingMessage => (this.message = incomingMessage)
     );
     this.directionService.isDirectionSet.subscribe(isDirectionSet => {
       this.isDirectionSet = isDirectionSet;
     });
-
-    //notifies component of language change to the App
-   translate.subscribeToAppLanguage(this.language);
   }
 
   ngOnInit() {
@@ -96,13 +92,17 @@ export class OutdoorNavigationToolbarComponent implements OnInit, AfterViewInit 
     });
   }
 
+  sendMessage(updatedMessage) {
+    this.data.updateMessage(updatedMessage);
+  }
+
   public changeCampus() {
-    this.dataSharing.updateMessage(this.locations[this.loc]);
+    this.sendMessage(this.locations[this.loc]);
     this.events.publish('campusChanged', Date.now());
   }
 
   public moveToFoundLocation(lat: number, lng: number) {
-    this.dataSharing.updateMessage({ latitude: lat, longitude: lng });
+    this.sendMessage({ latitude: lat, longitude: lng });
     this.events.publish('campusChanged', Date.now());
   }
 
