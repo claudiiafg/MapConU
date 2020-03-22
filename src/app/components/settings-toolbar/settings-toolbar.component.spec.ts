9bfad90758d8fe77560a78cd636f510a84c26426
KevinHTrinh <kevin--trinh@outlook.com>
2020-03-21 23:59:22 -0400

    added unit tests for settings-toolbar component

--- src/app/components/settings-toolbar/settings-toolbar.component.spec.ts
+++ src/app/components/settings-toolbar/settings-toolbar.component.spec.ts
@@ -1,38 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { RouteReuseStrategy, RouterModule } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { FirestoreSettingsToken } from '@angular/fire/firestore';
-import { SettingsToolbarComponent } from './settings-toolbar.component';
-import { UserServices } from '../../../services/user.services';
-import { PoiServices } from '../../../services/poi.services';
-import { GeolocationServices } from '../../../services/geolocation.services';
-import { DirectionService } from '../../../services/direction.service';
-import { IndoorDirectionsService } from '../../../services/indoorDirections.service';
-import { TranslationService } from '../../../services/translation.service';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { Geolocation } from "@ionic-native/geolocation/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { RouteReuseStrategy, RouterModule } from "@angular/router";
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { FirestoreSettingsToken } from "@angular/fire/firestore";
+import { SettingsToolbarComponent } from "./settings-toolbar.component";
+import { UserServices } from "../../../services/user.services";
+import { PoiServices } from "../../../services/poi.services";
+import { GeolocationServices } from "../../../services/geolocation.services";
+import { DirectionService } from "../../../services/direction.service";
+import { IndoorDirectionsService } from "../../../services/indoorDirections.service";
+import { TranslationService } from "../../../services/translation.service";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { By } from "@angular/platform-browser";
 
 //function that loads the external JSON files to the app using http-loader.
 export function LanguageLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, "assets/i18n/", ".json");
 }
 
-describe('SettingsToolbarComponent ', () => {
+describe("SettingsToolbarComponent ", () => {
   let component: SettingsToolbarComponent;
   let fixture: ComponentFixture<SettingsToolbarComponent>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterModule.forRoot([]), IonicModule.forRoot(),
+      imports: [
+        RouterModule.forRoot([]),
+        IonicModule.forRoot(),
         HttpClientModule,
         TranslateModule.forRoot({
           loader: {
             provide: TranslateLoader,
-            useFactory: (LanguageLoader),
+            useFactory: LanguageLoader,
             deps: [HttpClient]
           }
         })
@@ -59,8 +62,17 @@ describe('SettingsToolbarComponent ', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-  it('should create', () => {
+  it("should create", () => {
+    component.goBack();
     expect(component).toBeTruthy();
+  });
+  it("should test goBack()", () => {
+    fixture.detectChanges();
+    const spyPin = spyOn(component, "goBack");
+    let goBack = fixture.debugElement.query(By.css("ion-button.back-button"));
+    goBack.triggerEventHandler("click", null);
+    fixture.detectChanges();
+    expect(spyPin).toHaveBeenCalled();
   });
   afterEach(() => {
     TestBed.resetTestingModule();
