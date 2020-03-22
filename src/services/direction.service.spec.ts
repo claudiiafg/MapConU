025f83a52c1e1cfbd5dbddb0b8846a2a2e606496
miche-cho <michelle.choi@mail.mcgill.ca>
2020-03-22 17:12:31 -0400

    added service unit tests for direction service

--- src/services/direction.service.spec.ts
+++ src/services/direction.service.spec.ts
@@ -16,6 +16,44 @@ describe('DirectionService', () => {
     service.closeAlternateWindow();
     expect(service).toBeTruthy();
   });
+
+  it('should test set and get direction steps',() => {
+     const service: DirectionService = TestBed.get(DirectionService);
+     let stepsWithIcons = service.setStepsIcons(2);
+     service.setDirectionsSteps(stepsWithIcons);
+     service.getDirectionsSteps();
+     expect(service['directionSteps']).toBe(2);
+  });
+
+  it('should addInfoWindow', () => {
+      const service: DirectionService = TestBed.get(DirectionService);
+      service.addInfoWindow('testing window', 'Main');
+      expect(service['mainInfoWindow']).toBe('testing window');
+      expect(service['alternateInfoWindow']).toBeFalsy();
+      service.addInfoWindow('testing alternative window', 'Alternative');
+      expect(service['alternateInfoWindow']).toBeTruthy();
+      expect(service['alternateInfoWindow']).toBe('testing alternative window');
+  });
+
+  it('test setStepsIcons', () => {
+      const service: DirectionService = TestBed.get(DirectionService);
+      let stepsWithIcons = [
+          {travel_mode: 'WALKING', maneuver: 'left', travel_icon:'', maneuver_icon: ''},
+          {travel_mode: 'TRANSIT', maneuver: 'right', travel_icon:'', maneuver_icon: ''},
+          {travel_mode: 'TESTING1', maneuver: 'merge', travel_icon:'', maneuver_icon: ''},
+          {travel_mode: 'TESTING2', maneuver: 'left', travel_icon:'', maneuver_icon: ''}];
+      service.setStepsIcons(stepsWithIcons);
+
+      expect(stepsWithIcons[0].travel_icon).toBe('walk');
+      expect(stepsWithIcons[1].travel_icon).toBe('train');
+      expect(stepsWithIcons[2].travel_icon).toBe('car');
+      expect(stepsWithIcons[3].travel_icon).toBe('car');
+      expect(stepsWithIcons[0].maneuver_icon).toBe('arrow-round-back');
+      expect(stepsWithIcons[1].maneuver_icon).toBe('arrow-round-forward');
+      expect(stepsWithIcons[2].maneuver_icon).toBe('git-merge');
+      expect(stepsWithIcons[3].maneuver_icon).toBe('arrow-round-back');
+  });
+
   afterEach(() => {
     TestBed.resetTestingModule();
   });
