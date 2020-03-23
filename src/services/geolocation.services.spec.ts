b4ebdf1969fe582754fabfb26cc1e619ac3f0044
miche-cho <michelle.choi@mail.mcgill.ca>
2020-03-22 22:25:59 -0400

    added unit tests geolocation services

--- src/services/geolocation.services.spec.ts
+++ src/services/geolocation.services.spec.ts
@@ -14,6 +14,59 @@ describe('GeolocationServices', () => {
     const service: GeolocationServices = TestBed.get(GeolocationServices);
     expect(service).toBeTruthy();
   });
+
+  it('should get current position', () => {
+    const service: GeolocationServices = TestBed.get(GeolocationServices);
+    const mySpy = spyOn<any>(service, 'getCurrentPosition').and.callFake( ()  => {
+      service.latitude = 45.5;
+      service.longitude = -74.5;
+      return service.latitude, service.longitude;
+    });
+    service.getCurrentPosition();
+    expect(mySpy).toHaveBeenCalled();
+    expect(service.latitude).toEqual(45.5);
+    expect(service.longitude).toEqual(-74.5);
+  });
+
+  it('should get coordinates', () => {
+    const service: GeolocationServices = TestBed.get(GeolocationServices);
+    const mySpy = spyOn<any>(service, 'getCoordinates').and.callFake( ()  => {
+      service.latitude = 45.494828;
+      service.longitude = -73.577981;
+      return service.latitude, service.longitude;
+    });
+    service.getCoordinates();
+    expect(mySpy).toHaveBeenCalled();
+    expect(service.latitude).toEqual(45.494828);
+    expect(service.longitude).toEqual(-73.577981);
+  });
+
+  it('should get lat and long', () => {
+    const service: GeolocationServices = TestBed.get(GeolocationServices);
+    const latSpy = spyOn<any>(service, 'getLatitude').and.callFake( ()  => {
+      service.latitude = 45.494828;
+      return service.latitude;
+    });
+    const longSpy = spyOn<any>(service, 'getLongitude').and.callFake( ()  => {
+      service.longitude = -73.577981;
+      return service.longitude;
+    });
+    service.getLatitude();
+    service.getLongitude();
+    expect(latSpy).toHaveBeenCalled();
+    expect(longSpy).toHaveBeenCalled();
+    expect(service.latitude).toEqual(45.494828);
+    expect(service.longitude).toEqual(-73.577981);
+  });
+
+  it('should unsubscribe to position', () =>{
+    const service: GeolocationServices = TestBed.get(GeolocationServices);
+    const position = service.subscribeToPosition();
+    expect(service['geolocation']).toBeDefined();
+    service.unsuscribeToPosition();
+    expect(position).toBeFalsy();
+  });
+
   afterEach(() => {
     TestBed.resetTestingModule();
   });
