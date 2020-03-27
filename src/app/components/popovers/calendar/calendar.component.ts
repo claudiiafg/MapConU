a5555119de233849937223167c1491b8cde5b54f
Angel Chung <achung998@gmail.com>
2020-03-27 02:28:36 -0400

    Fetching of events from google calendar to populate calender added. Loading icon added. Ammount of recurring events limited to 3. Perfomarnace issues to be addressed.

--- src/app/components/popovers/calendar/calendar.component.ts
+++ src/app/components/popovers/calendar/calendar.component.ts
@@ -4,14 +4,9 @@ import {
 } from '@angular/core';
 
 import {
-  startOfDay,
-  endOfDay,
-  subDays,
-  addDays,
-  endOfMonth,
   isSameDay,
   isSameMonth,
-  addHours
+  format
 } from 'date-fns';
 
 import {
@@ -21,21 +16,10 @@ import {
 import { ModalController } from '@ionic/angular';
 import { HttpClientService } from '../../../../services/httpclient.service';
 import { GoogleOauthService } from '../../../../services/google-oauth.service';
-
-const colors: any = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3'
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF'
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA'
-  }
-};
+import { forkJoin, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { RRule, RRuleSet, rrulestr } from 'rrule'
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-calendar',
@@ -44,52 +28,29 @@ const colors: any = {
 })
 
 export class CalendarComponent implements OnInit {
-  private googleSession: any = null;
+  googleSession: any = null;
   view: CalendarView = CalendarView.Month;
   CalendarView = CalendarView;
   viewDate: Date = new Date();
   activeDayIsOpen: boolean = true;
 
-  private calendarsList: any;
+  eventsObservables: any[] = [];
+  calendarsList: any;
 
-  events: CalendarEvent[] = [
-    {
-      start: subDays(startOfDay(new Date()), 1),
-      end: addDays(new Date(), 1),
-      title: 'A 3 day event',
-      color: colors.red,
-      allDay: true
-    },
-    {
-      start: startOfDay(new Date()),
-      title: 'An event with no end date',
-      color: colors.yellow
-    },
-    {
-      start: subDays(endOfMonth(new Date()), 3),
-      end: addDays(endOfMonth(new Date()), 3),
-      title: 'A long event that spans 2 months',
-      color: colors.blue,
-      allDay: true
-    },
-    {
-      start: addHours(startOfDay(new Date()), 2),
-      end: addHours(new Date(), 2),
-      title: 'Another Event',
-      color: colors.yellow
-    }
-  ];
+  isReady = false;
+
+  events: CalendarEvent[] = [];
 
   constructor(private modalController: ModalController, private http: HttpClientService, private googleOAuth: GoogleOauthService) { }
 
   async ngOnInit() {
     try {
       this.googleSession = await this.googleOAuth.getStoredSession();
+      this.isReady = false;
+      this.getUserCalendarsRequest();
     } catch(err) {
       this.googleSession = null;
     }
-
-   this.getUserCalendarsRequest();
   }
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
@@ -104,6 +65,11 @@ export class CalendarComponent implements OnInit {
         }
         this.viewDate = date;
       }
+    }
+
+    //Directions logic in here
+    requestDirections(event: CalendarEvent) {
+      console.log(event);
     }
 
   setView(view: CalendarView) {
@@ -121,22 +87,32 @@ export class CalendarComponent implements OnInit {
     });
   }
 
-  getUserEventsRequest() {
+private getUserEventsRequest() {
     for(let i in this.calendarsList) {
-      try {
-        this.http.getEvents(this.googleSession, this.calendarsList[i].id).subscribe(data => {
-          this.events.push(...this.getEventToInsert(data['items'], this.calendarsList[i].backgroundColor));
-          console.log(this.events);
-        });
-      } catch(err) {
-        console.error("No events in calendar: " + this.calendarsList[i].id);
-      }
+      this.eventsObservables.push(this.http.getEvents(this.googleSession, this.calendarsList[i].id).pipe(
+        catchError(error => of(error))
+      ));
     }
+
+    forkJoin(this.eventsObservables).subscribe(
+      (res) => {
+        for(let i in res) {
+          if(!(res[i] instanceof HttpErrorResponse)) {
+            console.log(res[i]['items']);
+            this.events.push(...this.getEventToInsert(res[i]['items'], { primary: this.calendarsList[i].backgroundColor, secondary: this.calendarsList[i].backgroundColor }));
+          }
+        }
+
+        this.isReady = true;
+      },
+      (error) => console.log(error)
+    );
+
   }
 
 /*
 {
-  start: subDays(startOfDay(new Date()), 1),
+  start:  subDays(startOfDay(new Date()), 1),
   end: addDays(new Date(), 1),
   title: 'A 3 day event',
   color: colors.red,
@@ -146,16 +122,48 @@ export class CalendarComponent implements OnInit {
 
   private getEventToInsert(events: any, color: any) {
     let eventsList: CalendarEvent[] = [];
+    let entry: CalendarEvent;
+    let allDay: boolean;
+    let start: Date;
+    let end: Date;
 
     for(let event of events) {
-      let entry: CalendarEvent = {
-        start: new Date(event.start.dateTime),
-        end: new Date(event.end.dateTime),
-        title: event.summary,
-        color: color
+      if(event.start.dateTime) {
+          start = new Date(event.start.dateTime);
+          end = new Date(event.end.dateTime);
+          allDay = false;
+      } else {
+          start = new Date(event.start.date);
+          end = new Date(event.end.date);
+          allDay = true;
       }
-      eventsList.push(entry);
+
+      if(event.recurrence) {
+        let startRrule = rrulestr(`DTSTART;TZID=America/Adak:${format(start, "yyyyMMdd'T'HHmmss'Z'")}\n${event.recurrence[0]};COUNT=3`).all();
+        let endRrule = rrulestr(`DTSTART;TZID=America/Adak:${format(end, "yyyyMMdd'T'HHmmss'Z'")}\n${event.recurrence[0]};COUNT=3`).all()
+
+        for(let i in startRrule) {
+          entry  = {
+            start: startRrule[i],
+            end: endRrule[i],
+            title: event.summary,
+            color: color,
+            allDay: allDay
+          }
+          eventsList.push(entry);
+        }
+      } else {
+        entry  = {
+          start: start,
+          end: end,
+          title: event.summary,
+          color: color,
+          allDay: allDay
+        }
+        eventsList.push(entry);
+      }
     }
+
     return eventsList;
   }
 }
