89c4145b817e7639499f1358616de19be4231616
RobDrury <rdrury1513@hotmail.com>
2020-03-27 20:34:42 -0400

    Removed current location test due to limitation of chromeddrive. Refactored away from xpath

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -15,8 +15,8 @@ describe('new App', () => {
 it("Should toggle Campuses when selected at top of the UI", () => {
     // commenting this as it refreshes the page
     // can also sleep and wait until page fully loaded
-    // browser.get("/");
-
+    browser.get("/");
+    browser.driver.sleep(1000);
     //check location is set to SGW
     expect(element(by.id("campus-change")).getText()).toContain("SGW Campus");
     expect(element(by.css('ng-reflect-longitude="-73.578041"'))).toBeDefined();
@@ -63,22 +63,4 @@ it("Should toggle Campuses when selected at top of the UI", () => {
     expect(element(by.css('ng-reflect-longitude="-73.640452"'))).toBeDefined();
     expect(element(by.css('ng-reflect-latitude="45.45824"'))).toBeDefined();
   });
-  it('Shows Current Position Marker', () => {
-    browser.get('/');
-    let ele = element(by.xpath("/html/body/app-root/ion-app/ion-router-outlet/app-outdoor-view/ion-content/app-google-map/div/agm-map/div[1]/div/div/div[1]/div[3]/div/div[3]")).isPresent();
-    expect(ele).toBeTruthy();
-  });
-  //This test will be added in the future
-  // it("Shows Directions between point A and point B", () => {
-  //   browser.get('/');
-  //   element(by.xpath("/html/body/app-root/ion-app/app-outdoor-navigation-side-buttons/ion-fab[2]/ion-fab-button")).click();
-  //   browser.driver.sleep(500);
-  //   element(by.xpath("/html/body/app-root/ion-app/ion-popover/div/div[2]/app-search-popover/div[1]/input")).click()
-  //   element(by.xpath("/html/body/app-root/ion-app/ion-popover/div/div[2]/app-search-popover/div[1]/input")).sendKeys("1400 de maison");
-  //   element(by.xpath("/html/body/div[2]/div[1]")).click();
-  //   element(by.xpath("/html/body/app-root/ion-app/ion-popover/div/div[2]/app-search-popover/div[2]/input")).click()
-  //   element(by.xpath("/html/body/app-root/ion-app/ion-popover/div/div[2]/app-search-popover/div[2]/input")).sendKeys("1450 guy");
-  //   element(by.xpath("/html/body/div[2]/div[1]")).click();
-  //   expect(true).toBeTruthy;
-  // });
 });
