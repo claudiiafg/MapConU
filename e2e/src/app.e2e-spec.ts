529d1dfc23758f59f56c3cbf633d5e782ed3426a
RobDrury <rdrury1513@hotmail.com>
2020-03-28 13:36:55 -0400

    Added some tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,44 +1,42 @@
 import { AppPage } from './app.po';
 import { browser, by, element, $ } from 'protractor';
+import { protractor } from 'protractor/built/ptor';
 describe('new App', () => {
   let page: AppPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    browser.get("/");
+    browser.driver.sleep(200);
+
+    browser
+      .actions()
+      .mouseMove(element(by.css("button.alert-button")))
+      .click()
+      .perform();
+    browser.driver.sleep(200);
+
+    browser
+      .actions()
+      .mouseMove(element(by.css("button.alert-button")))
+      .click()
+      .perform();
+
+    browser.driver.sleep(200);
+
   });
 
   it('Has MapConU as the Title ', () => {
     browser.get('/');
-    let x = element(by.id('campus-change')).getText();
     expect(browser.getTitle()).toContain('MapConU');
   });
 it("Should toggle Campuses when selected at top of the UI", () => {
     // commenting this as it refreshes the page
     // can also sleep and wait until page fully loaded
-    browser.get("/");
-    browser.driver.sleep(1000);
+
     //check location is set to SGW
     expect(element(by.id("campus-change")).getText()).toContain("SGW Campus");
     expect(element(by.css('ng-reflect-longitude="-73.578041"'))).toBeDefined();
     expect(element(by.css('ng-reflect-latitude="45.495729"'))).toBeDefined();
-
-    //set location to Loyola
-    browser.driver.sleep(1000);
-
-    browser
-      .actions()
-      .mouseMove(element(by.css("button.alert-button")))
-      .click()
-      .perform();
-    browser.driver.sleep(1000);
-
-    browser
-      .actions()
-      .mouseMove(element(by.css("button.alert-button")))
-      .click()
-      .perform();
-
-    browser.driver.sleep(1000);
 
     browser
       .actions()
@@ -46,7 +44,7 @@ it("Should toggle Campuses when selected at top of the UI", () => {
       .click()
       .perform();
 
-    browser.driver.sleep(1000);
+    browser.driver.sleep(200);
 
     //set location to Loyola
     browser
@@ -54,7 +52,7 @@ it("Should toggle Campuses when selected at top of the UI", () => {
       .mouseMove(element(by.id("ion-rb-1-lbl")))
       .click()
       .perform();
-    browser.driver.sleep(1000);
+    browser.driver.sleep(200);
 
     //check locations is set to loyla
     expect(element(by.id("campus-change")).getText()).toContain(
@@ -62,5 +60,18 @@ it("Should toggle Campuses when selected at top of the UI", () => {
     );
     expect(element(by.css('ng-reflect-longitude="-73.640452"'))).toBeDefined();
     expect(element(by.css('ng-reflect-latitude="45.45824"'))).toBeDefined();
+  });
+it("Should open up the bus schedule", () => {
+  browser
+    .actions()
+    .mouseMove(element(by.id("bus-button")))
+    .click()
+    .perform();
+  expect(element(by.css('src="./assets/schedule/schedule.png"'))).toBeDefined();
+  });
+it("Should look up Hall building in the searchbar", () => {
+  //element(by.id("outdoor-search")).click();
+  browser.driver.sleep(200);
+  element(by.id("outdoor-search")).sendKeys("hall"+protractor.Key.ENTER);
   });
 });
