2108869dba2b49d20879756b221025034595b640
KevinHTrinh <kevin--trinh@outlook.com>
2020-03-29 21:30:54 -0400

    fixed dummy tests

---
+++ src/app/test/dummy.spec.ts
@@ -0,0 +1,11 @@
+describe("dummy ", () => {
+  it("dummy test 1+1", () => {
+    const hello = 1;
+    expect(hello * 2).toEqual(2);
+  });
+
+  // it('dummy test 2+2', () => {
+  //   const hello = 2;
+  //   expect(hello*2).toEqual(4);
+  // });
+});
