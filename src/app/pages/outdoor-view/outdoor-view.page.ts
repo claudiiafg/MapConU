d39d175e2901fe6ee96274cbc7af352f2f18b119
Claudia <claudia@bliinx.ca>
2020-04-12 21:54:49 -0400

    display / hide side buttons

--- src/app/pages/outdoor-view/outdoor-view.page.ts
+++ src/app/pages/outdoor-view/outdoor-view.page.ts
@@ -10,32 +10,30 @@ import { DataSharingService } from '../../../services/data-sharing.service';
 export class OutdoorViewPage implements OnInit {
   @ViewChild('toolbar', { static: false })
   toolbar: OutdoorNavigationToolbarComponent;
-  
+
   showSideButtons:Boolean = true;
   constructor(private router: Router, private dataSharingService: DataSharingService) {
-    
+
   }
 
-  ngOnInit() {    
-    this.dataSharingService.showSideButtons.subscribe(
-      (showSideButtons: boolean)  => {
-      this.showSideButtons = showSideButtons;
-      if (showSideButtons){
-        //this.show
-        console.log('not In 3d')
-        this.showSideButtons = true;
-      }
-      else{
-        //this.hide
-        console.log('In 3d')
-        this.showSideButtons = false;
-      }
-       console.log(this.showSideButtons);
-    });
-  3}
+  ngOnInit() {
+  }
 
   ngAfterViewInit(){
-
+    this.dataSharingService.showSideButtons.subscribe(
+      (showSideButtons: boolean)  => {
+      if (showSideButtons === true){
+        console.log('not In 3d')
+        this.showSideButtons = true;
+        document.getElementById('side-buttons').style.display = 'initial';
+      }
+      else{
+        console.log('In 3d')
+        this.showSideButtons = false;
+        document.getElementById('side-buttons').style.display = 'none';
+      }
+      console.log(this.showSideButtons);
+    });
   }
 
   navigate(path: []) {
