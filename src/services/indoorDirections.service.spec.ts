2eb0b36f6450f46213af53ed17244ea10e782b8b
KevinHTrinh <kevin--trinh@outlook.com>
2020-04-18 03:01:04 -0400

    WIP added several tests concerning paths and lines calculation tests

--- src/services/indoorDirections.service.spec.ts
+++ src/services/indoorDirections.service.spec.ts
@@ -200,13 +200,128 @@ describe("IndoorDirectionsService", () => {
     // should return true since we are checking two identical lines
     expect(service["sharePoint"](testLine, testLine)).toBeTruthy();
   });
-  // it("should test calcLength", () => {
-  //   const service: IndoorDirectionsService = TestBed.get(
-  //     IndoorDirectionsService
-  //   );
-  //   expect(service["calcLength"](5, 5, 15, 15)).toEqual(5);
-  //   expect(service["calcLength"](5, 15, 15, 15)).toEqual(5);
-  // });
+  it("should test calcLength", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    // calcLength(x1, x2, y1, y2): number
+    // return Math.max(y1, y2) - Math.min(y1, y2);
+    // 0 since y1 and y2 are equal
+    expect(service["calcLength"](5, 5, 15, 15)).toEqual(0);
+    // Math.max(x1, x2) - Math.min(x1, x2); so 10
+    expect(service["calcLength"](5, 15, 15, 15)).toEqual(10);
+  });
+
+  it("should test getPointInfoById", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    // there is no such point called temp, thus undefined
+    expect(service["getPointInfoById"]("temp")).toBeUndefined();
+  });
+
+  it("should test getPointOfLineById", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    // there is no such line called temp, thus undefined
+    expect(service["getPointOfLineById"]("temp")).toBeUndefined();
+  });
+  it("should test getInterestLineById", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    // there is no such point/line called temp, thus undefined
+    expect(service["getInterestLineById"]("temp")).toBeUndefined();
+  });
+  it("should test getLineById", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    service["pathLines"] = [testLine];
+    expect(service["getLineById"]("elevator")).toEqual(testLine);
+  });
+  it("should test getConnectedLineById", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    service["pathLines"] = [testLine];
+    // there's nothing connected, thus should return empty array
+    expect(service["getConnectedLineById"]("elevator")).toEqual([]);
+  });
+
+  it("should test setAsVisited", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    service["pathLines"] = [testLine];
+    service["setAsVisited"]("elevator");
+    // there's nothing connected, thus should return empty array
+    expect(testLine._wasVisited).toBeTruthy();
+  });
+
+  it("should test hasUnvisitedLine", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    // everything is not conected in our testline thus false,
+    // all is visited, no connected line
+    expect(service["hasUnvisitedLine"](testLine)).toBeFalsy();
+  });
+
+  it("should test getNextUnvisitedLine", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    // everything is not conected in our testline thus false,
+    // all is visited, no connected line, no next
+    expect(service["getNextUnvisitedLine"](testLine)).toBeFalsy();
+  });
+
+  it("should test countUnvisitedLines", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    // everything is not conected in our testline thus false,
+    // all is visited, no connected line, thus 0
+    expect(service["countUnvisitedLines"](testLine)).toEqual(0);
+  });
+
+  it("should test getUnvisitedLines", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    // everything is not conected in our testline thus false,
+    // all is visited, no connected line, thus []
+    expect(service["getUnvisitedLines"](testLine)).toEqual([]);
+  });
+
+  it("should test isLeaf", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    service["pathLines"] = [testLine];
+    // only one path line thus true since we know it's a leaf
+    expect(service["isLeaf"]("elevator")).toBeTruthy();
+  });
+
+  it("should test isIntersection", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    service["pathLines"] = [testLine];
+    // we know the value we are using will return true for this line intersection
+    expect(service["isIntersection"]("elevator")).toBeTruthy();
+  });
+
+  it("should test sharesLastLineWithPrevious", () => {
+    const service: IndoorDirectionsService = TestBed.get(
+      IndoorDirectionsService
+    );
+    // should return false with current values for our testLine
+    // only one line, this no previous possible
+    expect(service["sharesLastLineWithPrevious"](testLine)).toBeFalsy();
+  });
   afterEach(() => {
     TestBed.resetTestingModule();
   });
