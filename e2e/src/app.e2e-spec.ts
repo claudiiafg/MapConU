947dba1557c8a848b03d7ecb468b906d2c627a1c
RobDrury <rdrury1513@hotmail.com>
2020-04-19 13:26:25 -0400

    added tests for indoor directions

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -4,7 +4,7 @@ import { protractor } from 'protractor/built/ptor';
 var fs = require('fs')
 // abstract writing screen shot to a file
 function writeScreenShot(data, filename) {
-  var stream = fs.createWriteStream('./screenshots/'+filename);
+  var stream = fs.createWriteStream('./e2e/screenshots/'+filename);
   stream.write(new Buffer(data, 'base64'));
   stream.end();
 }
@@ -152,5 +152,70 @@ it("Should change the language of the outdoor page", () => {
   browser.takeScreenshot().then(function (png) {writeScreenShot(png, 'OutdoorPageinFrench.png');});
   expect(element(by.id("campus-change")).getText()).toContain("Campus SGW");
 
+  });
+it("Should load the indoor hall page", () => {
+  browser.get("/indoor/hall");
+  browser.takeScreenshot().then(function (png) {writeScreenShot(png, 'IndoorHallPage.png');});
+  expect(element(by.id("indoor-title"))).toBeDefined();
+  });
+it("Should give a path from h811 to h819", () => {
+  browser.get("/indoor/hall");
+  browser
+  .actions()
+  .mouseMove(element(by.id("button-nav")))
+  .click()
+  .perform();
+  //load start popover
+  browser.driver.sleep(400);
+  browser
+  .actions()
+  .mouseMove(element(by.id("indoor-poi-start")))
+  .click()
+  .perform();
+  browser.driver.sleep(400);
+  browser
+  .actions()
+  .mouseMove(element(by.id("alert-input-2-0")))
+  .click()
+  .perform();
+  browser
+  .actions()
+  .mouseMove(element(by.xpath("/html/body/app-root/ion-app/ion-alert/div/div[4]/button[2]")))
+  .click()
+  .perform();
+  //load destination popover
+  browser.driver.sleep(400);
+  browser
+  .actions()
+  .mouseMove(element(by.id("indoor-poi-end")))
+  .click()
+  .perform();
+  browser.driver.sleep(400);
+  browser
+  .actions()
+  .mouseMove(element(by.id("alert-input-3-4")))
+  .click()
+  .perform();
+  browser
+  .actions()
+  .mouseMove(element(by.xpath("/html/body/app-root/ion-app/ion-alert/div/div[4]/button[2]")))
+  .click()
+  .perform();
+  browser.driver.sleep(400);
+  browser
+  .actions()
+  .mouseMove(element(by.id("goButton")))
+  .click()
+  .perform();
+  browser.driver.sleep(400);  
+
+
+
+  browser.takeScreenshot().then(function (png) {writeScreenShot(png, 'IndoorHallPageWithPath.png');});
+  expect(element(by.id("line1")).getCssValue('stroke')).toBe('rgb(0, 0, 255)');
+  expect(element(by.id("line3")).getCssValue('stroke')).toBe('rgb(0, 0, 255)');
+  expect(element(by.id("line6")).getCssValue('stroke')).toBe('rgb(0, 0, 255)');
+  expect(element(by.id("line8")).getCssValue('stroke')).toBe('rgb(0, 0, 255)');
+  expect(element(by.id("line13")).getCssValue('stroke')).toBe('rgb(0, 0, 255)');
   });
 });
